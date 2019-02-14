de818adf09c6cd0091d826926dc323eb914f7deb
MikhailKochkin <44023454+MikhailKochkin@users.noreply.github.com>
2019-02-14 17:27:47 +0300

    Goals

--- savvy-front/components/sandbox/SandboxPage.js
+++ savvy-front/components/sandbox/SandboxPage.js
@@ -62,8 +62,8 @@ class CoursePage extends Component {
                         <>  
                             <HeaderStyles>
                                 <SandboxPageNav id={this.props.id}/>
-                                <SandBoxPageGoals 
-                                    id={this.props.id}/>
+                                {me !== null && me.id === sandbox.user && 
+                                <SandBoxPageGoals id={this.props.id}/>}
                             </HeaderStyles>
                             <div>
                                 {data1.sandboxes.map(sandbox => 
