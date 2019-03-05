e3c9cff5e1e890633188befce16c9dac367f4583
MikhailKochkin <44023454+MikhailKochkin@users.noreply.github.com>
2019-03-05 12:50:07 +0300

    +

--- savvy-back/src/index.js
+++ savvy-back/src/index.js
@@ -38,7 +38,7 @@ server.start(
   {
     cors: {
       credentials: true,
-      origin: [process.env.FRONTEND_URL3]
+      origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL2, process.env.FRONTEND_URL4]
     },
   },
   deets => {
