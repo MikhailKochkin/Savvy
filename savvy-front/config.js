76dec7e89e27ec4b320e4b730c2d83cacfccbdea
MikhailKochkin <44023454+MikhailKochkin@users.noreply.github.com>
2019-06-16 01:38:22 +0300

    +

--- savvy-front/config.js
+++ savvy-front/config.js
@@ -1,5 +1,5 @@
 export const endpoint = `http://localhost:4444`;
-export const prodEndpoint = `https://api.savvvy.app`;
+export const prodEndpoint = `https://savvy-yoga-production.herokuapp.com/`;
 export const CoursePerPage = 4;
 export const SandboxPerPage = 6;
 export const MaterialPerPage = 5;
