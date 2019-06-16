10930db2e80f2a95c92af730df0bd9632efe0a05
MikhailKochkin <44023454+MikhailKochkin@users.noreply.github.com>
2019-06-16 18:12:38 +0300

    +

--- savvy-back/src/index.js
+++ savvy-back/src/index.js
@@ -37,8 +37,8 @@ server.express.use(async (req, res, next) => {
 server.start(
   {
     cors: {
-      credentials: true,
-      origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL2, process.env.FRONTEND_URL3, process.env.FRONTEND_URL4]
+      credentials: false,
+      // origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL2, process.env.FRONTEND_URL3, process.env.FRONTEND_URL4]
     },
   },
   deets => {
