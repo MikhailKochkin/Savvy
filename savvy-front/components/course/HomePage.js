69f1b7cd189ac4c500d6f5e2a3595bf15566fda2
MikhailKochkin <44023454+MikhailKochkin@users.noreply.github.com>
2019-06-16 14:08:01 +0300

    v

---
+++ savvy-front/components/course/HomePage.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import Courses from './Courses';
+import Banner from '../Banner';
+import User from '../User';
+import CareerTrackMenu from '../career/CareerTrackMenu'
+import CareerTrackMap from '../career/CareerTrackMap'
+
+const HomeStyles = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`;
+
+const Menu = styled.div`
+    display: flex;
+    flex-direction: row;
+    @media (max-width: 700px) {
+        flex-direction: column;
+    }
+`;
+
+const CoursesStyles = styled.div`
+    flex-basis: 65%;
+    background-color: white;
+    border-radius: 2px;
+    @media (max-width: 700px) {
+        order: 2;
+    }
+`;
+
+const CareerStyles = styled.div`
+    flex-basis: 35%;
+    margin-left: 1rem;
+    @media (max-width: 700px) {
+        order: 1;
+    }
+`;
+
+class HomePage extends Component {
+    render() {
+        return (
+            <HomeStyles>
+              <User>
+                {({data: {me}}) => (  
+                    <>
+                        {/* Do not remove me !== null, otherwise " Cannot read property 'careerTrack' of null" */}
+                        {me === null && <Banner/>}
+                        {me !== null && me.careerTrack === null && <Banner/>}
+                        {me !== null && me.careerTrack !== null && <CareerTrackMap data={me.careerTrack.id}/>}
+                        <Menu>
+                            <CoursesStyles>
+                                <Courses me={me} page={this.props.page }/>
+                            </CoursesStyles>
+                            <CareerStyles>
+                                {!me && <CareerTrackMenu/> }
+                                {me && !me.careerTrack && <CareerTrackMenu me={me}/>}
+                                {me && me.careerTrack && <CareerTrackMenu me={me}/>}
+                            </CareerStyles>
+                        </Menu>
+                    </>
+                )}
+            </User>
+          </HomeStyles>
+        )
+    }
+}
+
+
+export default HomePage;
--- savvy-front/pages/index.js
+++ savvy-front/pages/index.js
@@ -1,69 +1,11 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
-import Courses from '../components/course/Courses';
-import Banner from '../components/Banner';
-import User from '../components/User';
-import CareerTrackMenu from '../components/career/CareerTrackMenu'
-import CareerTrackMap from '../components/career/CareerTrackMap'
-
-const HomeStyles = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-`;
-
-const Menu = styled.div`
-    display: flex;
-    flex-direction: row;
-    @media (max-width: 700px) {
-        flex-direction: column;
-    }
-`;
-
-const CoursesStyles = styled.div`
-    flex-basis: 65%;
-    background-color: white;
-    border-radius: 2px;
-    @media (max-width: 700px) {
-        order: 2;
-    }
-`;
-
-const CareerStyles = styled.div`
-    flex-basis: 35%;
-    margin-left: 1rem;
-    @media (max-width: 700px) {
-        order: 1;
-    }
-`;
+import HomePage from '../components/course/HomePage';
 
 class Home extends Component {
     render() {
         return (
-            <div>Стартовая страница</div>
-        //     <HomeStyles>
-        //       <User>
-        //         {({data: {me}}) => (  
-        //             <>
-        //                 {/* Do not remove me !== null, otherwise " Cannot read property 'careerTrack' of null" */}
-        //                 {me === null && <Banner/>}
-        //                 {me !== null && me.careerTrack === null && <Banner/>}
-        //                 {me !== null && me.careerTrack !== null && <CareerTrackMap data={me.careerTrack.id}/>}
-        //                 <Menu>
-        //                     <CoursesStyles>
-        //                         <Courses me={me} page={parseFloat(this.props.query.page) || 1 }/>
-        //                     </CoursesStyles>
-        //                     <CareerStyles>
-        //                         {!me && <CareerTrackMenu/> }
-        //                         {me && !me.careerTrack && <CareerTrackMenu me={me}/>}
-        //                         {me && me.careerTrack && <CareerTrackMenu me={me}/>}
-        //                     </CareerStyles>
-        //                 </Menu>
-        //             </>
-        //         )}
-        //     </User>
-        //   </HomeStyles>
-        )
+            <HomePage/>
+        )    
     }
 }
 
