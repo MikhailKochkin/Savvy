90c0ee4a57fce7a463485ee86555013fd84aafd5
MikhailKochkin <44023454+MikhailKochkin@users.noreply.github.com>
2019-08-17 18:50:22 +0300

    next update 9

--- savvy-front/pages/create.js
+++ savvy-front/pages/create.js
@@ -1,55 +1,54 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import CreateCourse from '../components/course/CreateCourse';
-import CreateSandbox from '../components/sandbox/CreateSandbox';
-import PleaseSignIn from '../components/auth/PleaseSignIn';
-
+import React, { Component } from "react";
+import styled from "styled-components";
+import CreateCourse from "../components/course/CreateCourse";
+import CreateSandbox from "../components/sandbox/CreateSandbox";
+import PleaseSignIn from "../components/auth/PleaseSignIn";
 
 const HomeStyles = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `;
 
 const Button = styled.button`
-    background-color: ${props => props.active ? "#122557" : "white"};
-    color: ${props => props.active ? "white" : "black"};
+  background-color: ${props => (props.active ? "#122557" : "white")};
+  color: ${props => (props.active ? "white" : "black")};
 `;
 
 const Buttons = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    Button {
-        width: 25%;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  button {
+    width: 25%;
+  }
+  @media (max-width: 800px) {
+    button {
+      font-size: 1rem;
+      width: 45%;
     }
-    @media (max-width: 800px) {
-        Button {
-            font-size: 1rem;
-            width: 45%;
-        }
+  }
+  @media (max-width: 500px) {
+    button {
+      font-size: 0.7rem;
+      display: inline-block;
+      /* width: 100%; */
     }
-    @media (max-width: 500px) {
-        Button {
-            font-size: 0.7rem;
-            display: inline-block;
-            /* width: 100%; */
-        }
-    }
+  }
 `;
 
 class CreateCoursePage extends Component {
   render() {
     return (
-        <div>
-          <PleaseSignIn>
+      <div>
+        <PleaseSignIn>
           <HomeStyles>
-              <CreateCourse/>
-            </HomeStyles>
-          </PleaseSignIn>
-        </div>
-    )
+            <CreateCourse />
+          </HomeStyles>
+        </PleaseSignIn>
+      </div>
+    );
   }
 }
 
--- savvy-front/pages/createPointA.js
+++
@@ -1,9 +0,0 @@
-import CreatePointA from '../components/create/CreatePointA';
-
-const CreatePointAPage  = props => (
-  <div>
-    <CreatePointA id={props.query.id} />
-  </div>
-);
-
-export default CreatePointAPage;
