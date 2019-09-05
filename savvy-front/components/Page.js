37d2bc020ac08e54ca1498fad95e989a9fb774ad
MikhailKochkin <44023454+MikhailKochkin@users.noreply.github.com>
2019-09-05 23:13:15 +0300

    roll back to styled components v3

--- savvy-front/components/Page.js
+++ savvy-front/components/Page.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
+import styled, { ThemeProvider, injectGlobal } from "styled-components";
 import { withRouter } from "next/router";
 import { ModalProvider } from "styled-react-modal";
 
@@ -30,6 +30,30 @@ const StyledPage = styled.div`
 const Inner = styled.div`
   margin: 0;
   width: 100%;
+`;
+
+injectGlobal`
+    html {
+      @import url('https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap&subset=cyrillic');
+        font-family: 'Montserrat', sans-serif;
+        box-sizing: border-box;
+        font-size: 10px;
+        height:100%;
+    }
+    *, *:after, *:after {
+        box-sizing: inherit;
+    }
+    body {
+        padding: 0;
+        margin: 0;
+        font-size:1.5rem;
+        line-height: 1.8;
+        height:100%;
+    }
+    a {
+        text-decoration: none;
+        color: ${theme.black};
+    }
 `;
 
 const Page = ({ children, router }) => {
--- savvy-front/pages/_app.js
+++ savvy-front/pages/_app.js
@@ -1,46 +1,8 @@
 import React from "react";
 import App, { Container } from "next/app";
 import { ApolloProvider } from "react-apollo";
-import { createGlobalStyle } from "styled-components";
 import Page from "../components/Page";
 import withData from "../lib/withData";
-
-const theme = {
-  blue: "#6DAAE1",
-  black: "#393939",
-  maxWidth: "1200px",
-  offWhite: "#EDEDED",
-  lightGrey: "#E1E1E1",
-  red: "#de6b48",
-  darkRed: "#ac2c05",
-  green: "#84BC9C",
-  darkGreen: "#294d4a",
-  yellow: "#FDF3C8"
-};
-
-const GlobalStyle = createGlobalStyle`
-    html {
-      @import url('https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap&subset=cyrillic');
-        font-family: 'Montserrat', sans-serif;
-        box-sizing: border-box;
-        font-size: 10px;
-        height:100%;
-    }
-    *, *:after, *:after {
-        box-sizing: inherit;
-    }
-    body {
-        padding: 0;
-        margin: 0;
-        font-size:1.5rem;
-        line-height: 1.8;
-        height:100%;
-    }
-    a {
-        text-decoration: none;
-        color: ${theme.black};
-    }
-`;
 
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
@@ -56,7 +18,6 @@ class MyApp extends App {
     const { Component, apollo, pageProps } = this.props;
     return (
       <Container>
-        <GlobalStyle />
         <ApolloProvider client={apollo}>
           <Page>
             <Component {...pageProps} />
