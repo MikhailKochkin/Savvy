c225d27ce2929cea08a239d7f9296f593ed7b6d2
MikhailKochkin <44023454+MikhailKochkin@users.noreply.github.com>
2019-09-09 21:20:16 +0300

    +

--- savvy-front/pages/index.js
+++ savvy-front/pages/index.js
@@ -1,11 +1,16 @@
 import Landing from "../components/Landing";
-import User from "../components/User";
 import Courses from "../components/course/Courses";
+import { CURRENT_USER_QUERY } from "../components/User";
+import { Query } from "react-apollo";
 
 const Index = () => (
-  <User>
-    {({ data: { me } }) => <>{me ? <Courses /> : <Landing me={me} />}</>}
-  </User>
+  <Query query={CURRENT_USER_QUERY}>
+    {({ data }, loading) => {
+      if (loading) return <p>Загрузка...</p>;
+      const me = data.me;
+      return <>{me ? <Courses /> : <Landing me={data.me} />}</>;
+    }}
+  </Query>
 );
 
 export default Index;
