d2c6bc3f36bbbeec3e765d8ea55245daa4b43292
MikhailKochkin <44023454+MikhailKochkin@users.noreply.github.com>
2019-11-10 22:37:39 +0300

    meta tags update

--- savvy-front/components/Meta.js
+++ savvy-front/components/Meta.js
@@ -1,15 +1,23 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 const Meta = () => (
-    <>
-        <Head>
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <meta charSet="utf-8" />
-            <link rel="shortcut icon" href="/static/favicon.ico" />
-            <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
-            <title>Savvy – платформа для управления юридическими знаниями</title>
-        </Head>
-    </>
-)
+  <>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="utf-8" />
+      <meta property="og:title" content="Savvvy App" />
+      <meta
+        property="og:description"
+        content="Интерактивные онлайн-курсы для юристов и актуальные вакансии"
+      />
+      <meta property="og:image" content="/static/mini-computer.jpg" />
+      <meta property="og:url" content="https://savvvy.app" />
+      <meta name="twitter:card" content="/static/mini-computer.jpg" />
+      <link rel="shortcut icon" href="/static/favicon.ico" />
+      <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
+      <title>Savvy – платформа для управления юридическими знаниями</title>
+    </Head>
+  </>
+);
 
 export default Meta;
