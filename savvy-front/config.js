0288ce8103a85b2a509cf0eb7fc3209281f4480a
MikhailKochkin <44023454+MikhailKochkin@users.noreply.github.com>
2020-04-27 12:29:01 +0300

    +

--- savvy-front/config.js
+++ savvy-front/config.js
@@ -1,6 +1,6 @@
 export const endpoint = `http://localhost:4444`;
 export const prodEndpoint =
-  `https://api.besavvy.app` || `https://api.savvvy.app`;
+  `https://api.savvvy.app` || `https://api.besavvy.app`;
 export const CoursePerPage = 6;
 export const SandboxPerPage = 6;
 export const MaterialPerPage = 5;
