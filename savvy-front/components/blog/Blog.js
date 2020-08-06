1161d8d5ed0068bf522458e4d9a9b3baea87be6e
MikhailKochkin <44023454+MikhailKochkin@users.noreply.github.com>
2020-08-06 16:15:15 +0300

    +

--- savvy-front/components/blog/Blog.js
+++ savvy-front/components/blog/Blog.js
@@ -141,9 +141,7 @@ const Blog = (props) => {
           )}
           <Query query={POSTS_QUERY}>
             {({ data, loading, fetchMore }) => {
-              if (loading) {
-                <p>Загрузка...</p>;
-              }
+              if (loading) return <p>Загрузка...</p>;
               return (
                 <>
                   {data.posts.map((d, index) => (
