814d81bf830ea4e70f00a52a6605735280d823b3
MikhailKochkin <44023454+MikhailKochkin@users.noreply.github.com>
2020-11-02 10:21:46 +0300

    +

--- savvy-front/components/lesson/problems/SingleProblem.js
+++ savvy-front/components/lesson/problems/SingleProblem.js
@@ -258,12 +258,13 @@ class SingleProblem extends Component {
                 </Mutation>
               </>
             )}
-            {(me && me.id === problem.user.id) ||
-              (me.permissions.includes("ADMIN") && !story && (
+            {me &&
+              (me.id === problem.user.id || me.permissions.includes("ADMIN")) &&
+              !story && (
                 <StyledButton onClick={(e) => this.setState({ update: true })}>
                   Изменить
                 </StyledButton>
-              ))}
+              )}
             {me && me.id === problem.user.id && !story ? (
               <DeleteSingleProblem
                 id={problem.id}
