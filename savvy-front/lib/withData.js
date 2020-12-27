0a42e2ba6dbfae795ca80776c1434501ed6c8dd8
MikhailKochkin <44023454+MikhailKochkin@users.noreply.github.com>
2020-12-27 12:55:24 +0300

    +

--- savvy-front/lib/withData.js
+++ savvy-front/lib/withData.js
@@ -19,8 +19,8 @@ function createClient({ headers, initialState }) {
       }),
       // this uses apollo-link-http under the hood, so all the options here come from that package
       createUploadLink({
-        uri: process.env.NODE_ENV === "development" ? endpoint : prodEndpoint,
-        // uri: prodEndpoint,
+        // uri: process.env.NODE_ENV === "development" ? endpoint : prodEndpoint,
+        uri: prodEndpoint,
         fetchOptions: {
           credentials: "include",
         },
