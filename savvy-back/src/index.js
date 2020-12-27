03e84d3fe4c2ff0fb804ca1188eb0feb2b50af60
MikhailKochkin <44023454+MikhailKochkin@users.noreply.github.com>
2020-12-27 12:50:30 +0300

    +

--- savvy-back/src/index.js
+++ savvy-back/src/index.js
@@ -31,8 +31,8 @@ app.use(cookieParser());
 
 app.use(async (req, res, next) => {
   const { token } = req.cookies;
-  console.log(0, req.cookies);
-  console.log(0, token);
+  console.log("req.headers.cookie", req.headers.cookie);
+  console.log("req.cookies", req.cookies);
   if (token) {
     const { userId } = jwt.verify(token, process.env.APP_SECRET);
     // put the userId onto the req for future requests to access
