0c402916279f6e3e3a81282f900684335f902c43
MikhailKochkin <44023454+MikhailKochkin@users.noreply.github.com>
2020-12-28 16:24:27 +0300

    +

--- savvy-front/next.config.js
+++ savvy-front/next.config.js
@@ -3,14 +3,3 @@ const withOptimizedImages = require("next-optimized-images");
 // const withCSS = require("@zeit/next-css");
 
 module.exports = withOptimizedImages();
-
-const { nextI18NextRewrites } = require("next-i18next/rewrites");
-
-const localeSubpaths = {};
-
-module.exports = {
-  rewrites: async () => nextI18NextRewrites(localeSubpaths),
-  publicRuntimeConfig: {
-    localeSubpaths,
-  },
-};
