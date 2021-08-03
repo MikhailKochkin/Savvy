fc1a55a2b5a286974ea9e2fcf20540d0b486498c
MikhailKochkin <44023454+MikhailKochkin@users.noreply.github.com>
2021-08-03 20:43:13 +0300

    +

--- savvy-front/pages/coursePage.js
+++ savvy-front/pages/coursePage.js
@@ -2,7 +2,8 @@ import CoursePage from "../components/course/CoursePage";
 import NewCoursePage from "../components/course/NewCoursePage";
 
 const CoursePagePage = (props) => {
-  return props.query.id === "cjtreu3md00fp0897ga13aktp" ? (
+  return props.query.id === "cjtreu3md00fp0897ga13aktp" &&
+    props.query.type !== "old" ? (
     <NewCoursePage id={props.query.id} />
   ) : (
     <CoursePage id={props.query.id} />
