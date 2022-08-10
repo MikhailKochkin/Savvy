6ab13fc621e25af4b8cd053524ca168676a834d8
MikhailKochkin <44023454+MikhailKochkin@users.noreply.github.com>
2022-08-11 00:31:55 +0400

    +

---
+++ savvy-front/pages/change.js
@@ -0,0 +1,9 @@
+// import UpdateCoursePage from "../components/course/UpdateCoursePage";
+
+// const update = ({ query }) => <UpdateCoursePage id={query.id} />;
+
+// export default update;
+
+const change = () => <div>Hello World</div>;
+
+export default change;
--- savvy-front/pages/update.js
+++
@@ -1,5 +0,0 @@
-import UpdateCoursePage from "../components/course/UpdateCoursePage";
-
-const update = ({ query }) => <UpdateCoursePage id={query.id} />;
-
-export default update;
