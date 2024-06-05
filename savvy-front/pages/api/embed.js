4aaf026305478ee5a2fd971d70bf5f0e7bcd544f
Mikhail <mikhail@Mikhails-MacBook-Pro.local>
2024-06-05 22:06:12 +0400

    add embed logs

--- savvy-front/pages/api/embed.js
+++ savvy-front/pages/api/embed.js
@@ -1,21 +1,37 @@
 export default function handler(req, res) {
-  const { simulatorId } = req.query;
-  console.log("simulatorId", simulatorId, req.query);
+  try {
+    const { simulatorId } = req.query;
+    console.log("Request query:", req.query);
 
-  // Check if the requested URL is valid and corresponds to a page in your app
+    // Check if the requested simulatorId is valid
+    if (!simulatorId) {
+      console.log("Invalid simulatorId");
+      return res.status(404).send({ error: "Invalid simulatorId" });
+    }
 
-  // Prepare the oEmbed response object
-  const oEmbedResponse = {
-    type: "rich", // or 'video', 'photo', etc.
-    version: "1.0",
-    title: "BeSavvy Simulator",
-    author_name: "Mike",
-    provider_name: "BeSavvy",
-    provider_url: "https://besavvy.app",
-    cache_age: "86400", // Cache age in seconds
-    html: `<iframe src="https://besavvy.app/embedPage?id=${simulatorId}" width="800" height="600"></iframe>`, // Replace with your embed code
-  };
+    console.log("Valid simulatorId:", simulatorId);
 
-  res.setHeader("Content-Type", "application/json");
-  res.status(200).json(oEmbedResponse);
+    // Prepare the oEmbed response object
+    const oEmbedResponse = {
+      type: "rich", // or 'video', 'photo', etc.
+      version: "1.0",
+      title: "BeSavvy Simulator",
+      author_name: "Mike",
+      provider_name: "BeSavvy",
+      provider_url: "https://besavvy.app",
+      cache_age: "86400", // Cache age in seconds
+      html: `<iframe src="https://besavvy.app/embeddedPage?id=${simulatorId}" width="800" height="600"></iframe>`, // Replace with your embed code
+    };
+
+    console.log("oEmbedResponse:", oEmbedResponse);
+
+    res.setHeader("Access-Control-Allow-Origin", "*"); // CORS header for debugging purposes
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json(oEmbedResponse);
+  } catch (error) {
+    console.error("Error occurred:", error);
+    res
+      .status(500)
+      .send({ error: "Internal Server Error", details: error.message });
+  }
 }
