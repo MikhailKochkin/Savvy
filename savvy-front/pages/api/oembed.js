50b676da0360fb8cd58ad7cea5d6bf3181af5a65
Mikhail <mikhail@Mikhails-MacBook-Pro.local>
2024-06-05 22:18:26 +0400

    +

--- savvy-front/pages/api/oembed.js
+++ savvy-front/pages/api/oembed.js
@@ -1,7 +1,10 @@
 export default function handler(req, res) {
   try {
-    const { simulatorId } = req.query;
+    const { url } = req.query;
+    const urlParams = new URLSearchParams(url.replace(/\?/g, "&")); // Convert query string to URLSearchParams
+    const simulatorId = urlParams.get("simulatorId");
     console.log("Request query:", req.query);
+    console.log("Extracted simulatorId:", simulatorId);
 
     // Check if the requested simulatorId is valid
     if (!simulatorId) {
@@ -13,19 +16,19 @@ export default function handler(req, res) {
 
     // Prepare the oEmbed response object
     const oEmbedResponse = {
-      type: "rich", // or 'video', 'photo', etc.
+      type: "rich",
       version: "1.0",
       title: "BeSavvy Simulator",
       author_name: "Mike",
       provider_name: "BeSavvy",
       provider_url: "https://besavvy.app",
-      cache_age: "86400", // Cache age in seconds
-      html: `<iframe src="https://besavvy.app/embeddedPage?id=${simulatorId}" width="800" height="600"></iframe>`, // Replace with your embed code
+      cache_age: "86400",
+      html: `<iframe src="https://besavvy.app/embeddedPage?id=${simulatorId}" width="800" height="600"></iframe>`,
     };
 
     console.log("oEmbedResponse:", oEmbedResponse);
 
-    res.setHeader("Access-Control-Allow-Origin", "*"); // CORS header for debugging purposes
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Content-Type", "application/json");
     res.status(200).json(oEmbedResponse);
   } catch (error) {
