5a171faa8534177818c05b1073b1226eefc95fa3
Mikhail <mikhail@Mikhails-MacBook-Pro.local>
2024-12-06 16:00:29 +0400

    clean the project strucutre from old projects

---
+++ savvy-front/pages/test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import DAGComponent from "../components/lesson/block_type_systems/DAGComponent";
+
+const test = () => {
+  return <DAGComponent />;
+};
+
+export default test;
